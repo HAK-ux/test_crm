2a41dff4d2b0e1c9c7c6ec42100b3171b911f7bb
Haider <khanh3777@gmail.com>
2026-02-02 18:14:20 -0700

    Added details api views

--- api/views.py
+++ api/views.py
@@ -46,3 +46,69 @@ def order_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET", "PUT", "POST"])
+def restaurant_detail(request, pk: int):
+    try:
+        restaurant = Restaurant.objects.get(pk=pk)
+    except Restaurant.DoesNotExist:
+        return Response("Not found.", status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == "GET":
+        serializer = RestaurantSerializer(restaurant)
+        return Response(serializer.data)
+    
+    elif request.method == "PUT":
+        serializer = RestaurantSerializer(restaurant, data=request.data)
+        if serializer.is_valid():
+            serializer.save() # updates existing restaurant
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == "DELETE":
+        restaurant.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(["GET", "PUT", "DELETE"])
+def customer_detail(request, pk: int):
+    try:
+        customer = Customer.objects.get(pk=pk)
+    except Customer.DoesNotExist:
+        return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = CustomerSerializer(customer)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = CustomerSerializer(customer, data=request.data)
+        if serializer.is_valid():
+            serializer.save()  # updates existing customer
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        customer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(["GET", "PUT", "DELETE"])
+def order_detail(request, pk: int):
+    try:
+        order = Order.objects.select_related("restaurant", "customer").get(pk=pk)
+    except Order.DoesNotExist:
+        return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = OrderSerializer(order, data=request.data)
+        if serializer.is_valid():
+            serializer.save()  # updates existing order
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        order.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
